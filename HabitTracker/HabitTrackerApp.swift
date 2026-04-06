01868000565ea5d79ab0645dd2aaab07d2b2c636
gokdenizinan <gokdeniz.inan@wur.nl>
2026-04-06 15:19:49 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Gökdeniz İnan on 6.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
