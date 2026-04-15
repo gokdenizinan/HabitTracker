930fc9c1ea51aa5b46afb26af52392b91f237e44
gokdenizinan <gokdeniz_inan@hotmail.com>
2026-04-15 23:57:31 +0200

    update event function now works in the ui

--- HabitTracker/Features/Habits/Views/ContentView.swift
+++ HabitTracker/Features/Habits/Views/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     // Storage
     @State var  newHabitName: String = ""
     @State var newHabitPoint: String = ""
+    @State var editingHabitID: UUID? = nil
+    @State var editedHabitName: String = ""
+    @State var editedHabitPoint: String = ""
+    
     @StateObject var viewModel = HabitsViewModel()
     
     var body: some View {
@@ -20,29 +24,59 @@ struct ContentView: View {
                 Text("My Habits").font(Font.custom("Geist-Regular", size: 50)).padding(.top,100)
                 List{
                     ForEach($viewModel.habitsArray) { $habit in
-                        HStack (alignment: .center, spacing: 20){
-                            Text(habit.name)
-                                .padding(.leading)
-                            Text(String(habit.point))
-                            Spacer()
-                            Image(systemName: habit.isComplete ? "checkmark.square.fill":"square")
-                                .font(.system(size:30))
-                                .foregroundStyle(habit.isComplete ? .green: .gray)
+                        
+                        if editingHabitID == habit.id{
+                            TextField("Edit name", text: $editedHabitName)
+                            TextField("Edit point", text: $editedHabitPoint)
+                            
+                            Button("Save"){
+                                viewModel.updateHabit(habit: habit, difHabitName: editedHabitName, difHabitPoint: editedHabitPoint)
+                                editingHabitID = nil
+                            }
+                            Button("Cancel"){
+                                editingHabitID = nil
+                            }
+                            
                         }
-                        .swipeActions(edge: .leading){
-                            Button(role: .destructive) {
-                                viewModel.deleteHabit(habit: habit)
-                            } label: {
-                                Label("Delete", systemImage: "trash")
+                        else{
+                            HStack (alignment: .center, spacing: 20){
+                                Text(habit.name)
+                                    .padding(.leading)
+                                Text(String(habit.point))
+                                Spacer()
+                                Image(systemName: habit.isComplete ? "checkmark.square.fill":"square")
+                                    .font(.system(size:30))
+                                    .foregroundStyle(habit.isComplete ? .green: .gray)
+                                    .swipeActions(edge: .leading){
+                                        Button(role: .destructive) {
+                                            viewModel.deleteHabit(habit: habit)
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
+                                        }
+                                        Button{
+                                            editingHabitID = habit.id
+                                            editedHabitName = habit.name
+                                            editedHabitPoint = String(habit.point)
+                                        } label: {
+                                            Label("Change" ,systemImage: "square.and.pencil")
+                                        }.tint(.yellow)
+                                    }
+                                    .swipeActions(edge: .trailing){
+                                        Button {
+                                            habit.isComplete.toggle()
+                                        } label: {
+                                            if habit.isComplete == true
+                                             {
+                                                 Label("Undone", systemImage: "bolt.slash")
+                                             }
+                                             else {
+                                                 Label("Done" , systemImage: "bolt.fill")
+                                             }
+                                        }
+                                    }
                             }
                         }
-                        .swipeActions(edge: .trailing){
-                            Button {
-                                habit.isComplete.toggle()
-                            } label: {
-                                Label("Toggle", systemImage: "checkmark")
-                            }
-                        }
+                        
                     }
                     TextField("Enter new habit: ", text: $newHabitName)
                         .padding(.top)
@@ -78,4 +112,3 @@ struct ContentView: View {
 #Preview() {
     ContentView()
 }
-
